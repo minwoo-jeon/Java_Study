c3b198c44076f4007e48755a5ea553b60284f171
Min Woo Jeon <jeonminwoo2000@gmail.com>
2023-12-07 18:41:11 +0900

    first commit

--- src/loop/ex/ex07.java
+++ src/loop/ex/ex07.java
@@ -2,7 +2,7 @@ package loop.ex;
 
 public class ex07 {
     public static void main(String[] args) {
-        //문제: 피라미드 출력 int rows를 선언해라.이 수만큼 다음과 같은 피라미드를 출력하면 된다.
+        //문제: 피라미드 출력 int rows를 선언해라.이 수만큼 다음과 같은 피라미드를 출력하면 됩니다.
         int row = 2;
         for(int i=1; i<=row;i++){
             System.out.println("*");
